72f89f88b2672af6e7bbacbfbd7fbac0a101bcbd
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2024-07-30 05:50:09 -0300

    aula 58

---
+++ src/paginas/login.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Header from "../componentes/header.svelte";
+	import Menu from "../componentes/menu.svelte";
+	import Footer from "../componentes/footer.svelte";
+</script>
+
+<Header></Header>
+<Menu></Menu>
+<section class="page-section cta">
+	<div class="container">
+		<div class="row">
+			<div class="col-xl-9 mx-auto">
+				<div class="cta-inner bg-faded text-center rounded">
+					<h2 class="section-heading mb-4">
+						<span class="section-heading-upper">Sobre o desafio de front-end agora com Svelte</span>
+					</h2>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<Footer></Footer>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import Home from './paginas/home.svelte';
 import Sobre from './paginas/sobre.svelte';
+import Login from './paginas/login.svelte';
 import Produtos from './paginas/produtos/produtos.svelte';
 import NotFound from './paginas/notFound.svelte';
 
@@ -7,6 +8,7 @@ const routes = [
   { path: '/', component: Home },
   { path: '/produtos', component: Produtos },
   { path: '/sobre', component: Sobre },
+  { path: '/login', component: Login },
   { path: '*', component: NotFound },
 ];
 
